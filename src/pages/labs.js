a17c8bc55bf689b87c8409975119e2e237a6c907
mayconbalves <mayconmustaine@hotmail.com>
2021-04-13 21:49:46 -0300

    feat(pages): create labs page

---
+++ src/pages/labs.js
@@ -0,0 +1,13 @@
+// https://codepen.io/malves/pen/QYqNdK?editors=0110
+import React from 'react'
+import Content from '../components/content'
+
+const Labs = () => {
+  return (
+    <Content>
+      <h1>hello labs</h1>
+    </Content>
+  )
+}
+
+export default Labs
