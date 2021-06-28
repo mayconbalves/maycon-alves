85760a368a040acda20c9bf71d3ee5d926ee4083
mayconbalves <mayconmustaine@hotmail.com>
2021-06-28 15:35:05 -0300

    refactor: update document for styled component

--- src/pages/_documents.js
+++ src/pages/_documents.js
@@ -1,30 +1,30 @@
-import Document from 'next/document'
+import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />)
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        )
+    function handleCollectStyles(App) {
+      return (props) => {
+        return sheet.collectStyles(<App {...props} />)
       }
-    } finally {
-      sheet.seal()
     }
+
+    const page = renderPage((App) => handleCollectStyles(App))
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
   }
 }
